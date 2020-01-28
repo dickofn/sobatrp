91b80b102367403d9bf59fd1df2a28e1ea8783f5
Dicko Finus <dickofn@gmail.com>
2020-01-28 18:35:56 +0700

    Added custom scss variable

--- src/main.js
+++ src/main.js
@@ -6,6 +6,8 @@ import BootstrapVue from "bootstrap-vue";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-vue/dist/bootstrap-vue.css";
 
+import "~/assets/scss/custom.scss";
+
 import DefaultLayout from "~/layouts/Default.vue";
 
 export default function(Vue, { router, head, isClient }) {
